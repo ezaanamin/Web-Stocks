98fcf17b95e98fddf8e5126bedc13a7102536168
Ezaan Amin <54580594+ezaanamin1@users.noreply.github.com>
2021-09-19 10:51:00 +0500

    Add files via upload

--- stocksmarket/urls.py
+++ stocksmarket/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
-from .import views
+app='stocksmarkets'
 urlpatterns = [
-    path('homepage/',views.home,name='home'),
-    path("company/",views.homepage,name="homepage"),
-    path('serach/',views.mainpage,name='mainpage'),
-    
-
+    path('admin/', admin.site.urls),
+    path('main/',include('stocksmarket.urls')),
 ]
