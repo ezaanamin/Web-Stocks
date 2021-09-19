17ec0e588182edb8f52f5f5a5441651549962bd7
Ezaan Amin <54580594+ezaanamin1@users.noreply.github.com>
2021-09-18 22:54:15 -0700

    Update urls.py

--- Stocks/urls.py
+++ Stocks/urls.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+app='stocksmarkets'
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('main/',include('stocksmarket.urls')),
+]
 
